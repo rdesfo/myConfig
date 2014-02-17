6c843819fc3d7b8a0d2770663d38026131c6aa60
Ryan Desfosses <ryan@desfo.org>
2014-02-17 17:08:20 -0500

    updated xfce settings

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -1,24 +1,25 @@
 Config { font = "-misc-fixed-10-*-*-*-*-*-*-*-*"
-       , bgColor = "black"
+       , bgColor = "#242424"
        , fgColor = "grey"
        , border = TopB
-       , borderColor = "black"
+       , borderColor = "#242424"
        , allDesktops = False
        , overrideRedirect = False
        , hideOnStart = False
        , persistent = True
-       , position = TopW L 91
+       , position = TopSize L 91 20
        , lowerOnStart = False
        , commands = [ Run Weather "KPWM" ["-t","Portland: <tempF>F  <skyCondition>","-L","32","-H","80","--normal","green","--high","red","--low","lightblue"] 36000
                     , Run MultiCpu ["-t","Cpu: <autototal>%"] 10
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Swap [] 10
-                    , Run Battery ["BAT0"] 10
+--                    , Run BatteryP ["BAT0"] ["-t", "Bat: <acstatus><watts> (<left>%)", "-L", "10", "-H", "80", "-p", "3", "--", "-O", "<fc=green>On</fc> - ", "-i", "","-L", "-15", "-H", "-5","-l", "red", "-m", "blue", "-h", "green"] 10
                     , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
                     , Run StdinReader
                     , Run Com "kquitapp" ["plasma-desktop", "&&", "sleep", "15s", "&&", "plasma-desktop"] "" 0
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %multicpu% | %memory% * %swap% | %battery% | %KPWM% | <fc=#ee9a00>%date%</fc>"
+--       , template = "%StdinReader% }{ %multicpu% | %memory% * %swap% | %battery | %KPWM% | <fc=#ee9a00>%date%</fc>"
+       , template = "%StdinReader% }{ %multicpu% | %memory% * %swap% | %KPWM% | <fc=#ee9a00>%date%</fc>"
        }
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,7 +6,7 @@ import XMonad.Hooks.DynamicLog     -- statusbar
 import XMonad.Actions.CycleWS      -- workspace-switching
 import XMonad.Util.EZConfig        -- append key/mouse bindings
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Config.Kde
+import XMonad.Config.Xfce
 import XMonad.Layout
 import XMonad.Layout.NoBorders ( noBorders, smartBorders )
 import XMonad.Layout.Spacing  
@@ -17,27 +17,23 @@ import System.IO
 -- and click on the client you're interested in.
 
 myManageHook = composeAll
-  [ className =? "yakuake"        --> doFloat
-  , className =? "Yakuake"        --> doFloat
-  , className =? "Kmix"           --> doFloat
-  , className =? "kmix"           --> doFloat
-  , className =? "plasma"         --> doFloat
-  , className =? "Plasma"         --> doFloat
-  , className =? "plasma-desktop" --> doFloat
-  , className =? "Plasma-desktop" --> doFloat
-  , className =? "krunner"        --> doFloat
-  , className =? "ksplashsimple"  --> doFloat
-  , className =? "ksplashqml"     --> doFloat
-  , className =? "ksplashx"       --> doFloat
+  [ className =? "xfdesktop"      --> doFloat
+  , className =? "Xfdesktop"      --> doFloat
+  , className =? "xfce4-panel"    --> doFloat
+  , className =? "Xfce4-panel"    --> doFloat
+  , className =? "xfdesktop"      --> doIgnore
+  , className =? "Xfdesktop"      --> doIgnore
   , className =? "stalonetray"    --> doIgnore
-  , className =? "VirtualBox"     --> doShift "4:vm"
+  , className =? "VirtualBox"     --> doShift "6:vm"
   , className =? "steam"          --> doShift "5:media"
   , className =? "Steam"          --> doShift "5:media"
   , className =? "pidgin"         --> doShift "4:im"
   , className =? "Pidgin"         --> doShift "4:im"
   , className =? "Thunderbird"    --> doShift "3:mail"
+  , className =? "Mail"           --> doShift "3:mail"
   , className =? "vlc"            --> doShift "7:media"
-  , className =? "kaffiene"            --> doShift "7:media"
+  , className =? "Vlc"            --> doShift "7:media"
+  , className =? "kaffiene"       --> doShift "7:media"
   ]
 
 myLayout = tiled ||| Mirror tiled ||| Full
@@ -49,7 +45,7 @@ myLayout = tiled ||| Mirror tiled ||| Full
     ratio = 1/2 -- Default proportion of screen occupied by master pane
     delta = 5/100  -- % of screen to increment by when resizing panes
   
-myWorkspaces = ["1:grnd0","2:web","3:mail","4:im","5:", "6:","7:media","8:games","9:steam"]
+myWorkspaces = ["1:grnd0","2:web","3:mail","4:im","5:", "6:vm","7:media","8:games","9:steam"]
 
 logHook' xmproc =
   dynamicLogWithPP $ xmobarPP
@@ -59,15 +55,15 @@ logHook' xmproc =
 
 main = do
   xmproc <- spawnPipe "killall xmobar ; xmobar ~/.xmonad/xmobar.hs"
-  traybar <- spawnPipe "killall stalonetray ; stalonetray -i 16 --max-geometry 8x1-0+0 --icon-gravity E --geometry 8x1-0+0 -bg black --sticky --skip-taskbar --config $HOME/.xmonad/stalonetrayrc"
+  traybar <- spawnPipe "killall stalonetray ; stalonetray -i 20 --max-geometry 8x1-0+0 --icon-gravity E --geometry 8x1-0+0 -bg '#'242424 --sticky --skip-taskbar"
   xmonad $ ewmh desktopConfig 
     { modMask = mod4Mask
-    , terminal = "konsole"
+    , terminal = "xfce4-terminal"
     , manageHook = manageDocks <+> myManageHook <+> manageHook desktopConfig
     , borderWidth = 1
     , normalBorderColor = "#abc123"
     , focusedBorderColor = "#456def"
-    , layoutHook = avoidStruts myLayout
+    , layoutHook = smartBorders . avoidStruts $ myLayout
     , workspaces = myWorkspaces
     , startupHook = startupHook desktopConfig
     , logHook     = logHook' xmproc
