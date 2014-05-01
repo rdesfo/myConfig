cfd29b989b6d730b36573746f3d37719dd6add6d
Ryan Desfosses <ryan@desfo.org>
2014-05-01 15:47:47 -0400

    updated xmobar

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -7,19 +7,19 @@ Config { font = "-misc-fixed-10-*-*-*-*-*-*-*-*"
        , overrideRedirect = False
        , hideOnStart = False
        , persistent = True
-       , position = TopSize L 91 20
+       , position = TopSize L 100 20
        , lowerOnStart = False
        , commands = [ Run Weather "KPWM" ["-t","Portland: <tempF>F  <skyCondition>","-L","32","-H","80","--normal","green","--high","red","--low","lightblue"] 36000
                     , Run MultiCpu ["-t","Cpu: <autototal>%"] 10
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Swap [] 10
---                    , Run BatteryP ["BAT0"] ["-t", "Bat: <acstatus><watts> (<left>%)", "-L", "10", "-H", "80", "-p", "3", "--", "-O", "<fc=green>On</fc> - ", "-i", "","-L", "-15", "-H", "-5","-l", "red", "-m", "blue", "-h", "green"] 10
+                    , Run BatteryP ["BAT0"] ["-t", "Bat: <left>%", "-L", "10", "-H", "80", "-p", "3", "--", "-O", "<fc=green>On</fc> - ", "-i", "","-L", "-15", "-H", "-5","-l", "red", "-m", "blue", "-h", "green"] 10
                     , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
                     , Run StdinReader
                     , Run Com "kquitapp" ["plasma-desktop", "&&", "sleep", "15s", "&&", "plasma-desktop"] "" 0
                     ]
        , sepChar = "%"
        , alignSep = "}{"
---       , template = "%StdinReader% }{ %multicpu% | %memory% * %swap% | %battery | %KPWM% | <fc=#ee9a00>%date%</fc>"
-       , template = "%StdinReader% }{ %multicpu% | %memory% * %swap% | %KPWM% | <fc=#ee9a00>%date%</fc>"
+       , template = "%StdinReader% }{ <fc=#ee9a00>%date%</fc>                     %multicpu% | %memory% * %swap% | %battery% | %KPWM% "
+--       , template = "%StdinReader% }{ %multicpu% | %memory% * %swap% | %KPWM% | <fc=#ee9a00>%date%</fc>"
        }
