8945175fb79f8442bc44a633c0fed36540909d31
Ryan Desfosses <ryan@desfo.org>
2017-08-04 22:53:14 -0400

    added init.el

---
+++ init.el
@@ -0,0 +1,33 @@
+
+(require 'package) 
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; bootstrap =use-package=
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(org-agenda-files (quote ("~/Documents/org/journal.org")))
+ '(package-selected-packages
+   (quote
+    (mu mu4e wanderlust docker-tramp docker-mode markdown-mode bbdb intero elfeed-goodies elfeed elfeed-org use-package try evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
